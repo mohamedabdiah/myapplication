c85756894b66d0f1ed58be5c0df99ef48b4c9f84
mohamedabdiah <mohamedabdikaniahmed@gmail.com>
2024-12-05 21:12:56 +0300

    we are solved the problem

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ const btn = document.getElementById("btn");
 btn.addEventListener("click",()=>{
       alert("Hello Clicked")
 })
+
+let age = 13;
+if(age >=15){
+      console.log("your a old")
+}else{
+      console.log("your a young")
+}
